249e3a390b83cda56f3ff945590ab50125e3d1a7
Alex Urraco Vert <alex.urraco@estudiantat.upc.edu>
2022-05-25 14:51:15 +0200

    Moviment fet

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -36,23 +36,18 @@ public class Movement : MonoBehaviour
     void Start()
     {
         turnLeft = turnRight = false;
-        it = 0;
+        //it = 0;
         _cc = GetComponent<CharacterController>();
         //_anim = GetComponent<Animator>();
-        positions = new Vector3[]{new Vector3(-2.831235f,0.8866265f,-55.69607f), new Vector3(-35.22f,-1.107f,-61.78f)};
-        finalActualPos = positions[it];
-        triggerPosition.OnContact += ChangePos;
+        //positions = new Vector3[]{new Vector3(-2.831235f,0.8866265f,-55.69607f), new Vector3(-35.22f,-1.107f,-61.78f)};
+        //finalActualPos = positions[it];
+        //triggerPosition.OnContact += ChangePos;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 movement = Vector3.zero;
-        /*
-        xToRun = finalPos.x - initialPos.x;
-        yToRun = finalPos.y - initialPos.y;
-        zToRun = finalPos.z - initialPos.z;
-*/
         //moure's amb el click esquerra del mouse
         if (Input.GetKey(KeyCode.Mouse0))
         {
@@ -63,13 +58,15 @@ public class Movement : MonoBehaviour
         //moure's a la dreta
         if (turnRight)
         {
-            transform.Rotate(new Vector3(0, 90f, 0f));
+            transform.Rotate(new Vector3(0, 45f, 0f));
+            turnRight = false;
         }
 
         //si rotem cap a la esquerra
         if (turnLeft)
         {
-            transform.Rotate(new Vector3(0, -90f, 0f));
+            transform.Rotate(new Vector3(0, -45f, 0f));
+            turnLeft = false;
         }
 
         /*
@@ -98,13 +95,13 @@ public class Movement : MonoBehaviour
 
     }
 
-    void OnDisable()
+    /*void OnDisable()
     {
         triggerPosition.OnContact -= ChangePos;
-    }
+    }*/
 
-    void OnCollisionEnter(Collision col)
+   /* void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "PlaneRight") goRight();
-    }
+    }*/
 }
--- Assets/Scripts/TriggerPosition.cs
+++ Assets/Scripts/TriggerPosition.cs
@@ -8,20 +8,8 @@ public class TriggerPosition : MonoBehaviour
     public delegate void Contact();
     public event Contact OnContact;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    private void OnColliderEnter(Collider other){   
-        if(OnContact != null)
-            OnContact();
-
+    private void OnTriggerEnter(Collider other){
+        other.gameObject.GetComponent<Movement>().goRight();
     }
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
