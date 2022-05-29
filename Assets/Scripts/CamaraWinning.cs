2f904ea66dd43e274c12a442a4b869f786548419
Alex Urraco Vert <alex.urraco@estudiantat.upc.edu>
2022-05-29 13:34:04 +0200

    final camara arreglada

--- Assets/Scripts/CamaraWinning.cs
+++ Assets/Scripts/CamaraWinning.cs
@@ -9,23 +9,57 @@ public class CamaraWinning : MonoBehaviour
     // Start is called before the first frame update
     public GameObject cameraP1;
     public GameObject cameraP2;
+    public GameObject VcameraP1;
+    public GameObject VcameraP2;
+    public GameObject finalCamP1;
+    public GameObject finalCamP2;
+    private CharacterController _cc;
+    private Camera c1;
+    private Camera c2;
+    private Camera fc;
     [SerializeField]
     private CinemachineVirtualCamera cvc;
     [SerializeField]
     private CinemachineTransposer ct;
     public void changeCamara(){
-        cameraP1 = GameObject.FindWithTag("VirtualCameraP1");
-        cameraP2 = GameObject.FindWithTag("VirtualCameraP2");
+        _cc = GetComponent<CharacterController>();
+        cameraP1 = GameObject.FindWithTag("MainCameraP1");
+        cameraP2 = GameObject.FindWithTag("MainCameraP2");
+        VcameraP1 = GameObject.FindWithTag("VirtualCameraP1");
+        VcameraP2 = GameObject.FindWithTag("VirtualCameraP2");
+        finalCamP1 = GameObject.FindWithTag("FinalCamP1");
+        finalCamP2 = GameObject.FindWithTag("FinalCamP2");
             if (gameObject.layer == 11){ //player 1
-                cvc = cameraP1.GetComponent<CinemachineVirtualCamera>();
+                cvc = VcameraP1.GetComponent<CinemachineVirtualCamera>();
                 cvc.m_Lens.FieldOfView = 20f;
-                transform.Rotate(new Vector3(0, 180f, 0f));
+                //transform.Rotate(new Vector3(0, 180f, 0f));
                 ct = cvc.GetCinemachineComponent<CinemachineTransposer>();
+               // Destroy(cameraP1);
+               
+                c1 = cameraP1.GetComponent<Camera>();
+                c2 = cameraP1.GetComponent<Camera>();
+                fc = finalCamP1.GetComponent<Camera>();
+                c1.enabled = false;
+                c2.enabled = false;
+                fc.enabled = true;
                // ct.m_BindingMode = ;
+               _cc.enabled = false;
         }
         else{ //player 2
-            cvc = cameraP2.GetComponent<CinemachineVirtualCamera>();
-            cvc.m_Lens.FieldOfView = 15f;
+             cvc = VcameraP1.GetComponent<CinemachineVirtualCamera>();
+                cvc.m_Lens.FieldOfView = 20f;
+                //transform.Rotate(new Vector3(0, 180f, 0f));
+                ct = cvc.GetCinemachineComponent<CinemachineTransposer>();
+               // Destroy(cameraP1);
+               
+                c1 = cameraP1.GetComponent<Camera>();
+                c2 = cameraP1.GetComponent<Camera>();
+                fc = finalCamP2.GetComponent<Camera>();
+                c1.enabled = false;
+                c2.enabled = false;
+                fc.enabled = true;
+               // ct.m_BindingMode = ;
+               _cc.enabled = false;
 
         }
     }
