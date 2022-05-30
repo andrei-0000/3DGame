67924e68fada94ad2fd1997422e20216469603b8
Alex Urraco Vert <alex.urraco@estudiantat.upc.edu>
2022-05-30 20:25:01 +0200

    nivells acabats

---
+++ Assets/Scripts/FallPlat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallPlat : MonoBehaviour
+{
+	public float fallTime = 0.5f;
+
+	Renderer myR;
+    Collider myC;
+
+
+	void OnCollisionEnter(Collision collision)
+     {
+        myR = GetComponent<Renderer>();
+        myC = GetComponent<Collider>();
+        StartCoroutine(Fall(fallTime));
+        StartCoroutine(Reapper());
+    
+     }
+
+	IEnumerator Fall(float time)
+	{
+		yield return new WaitForSeconds(time);
+		myR.enabled = false;
+        myC.enabled = false;
+	}
+
+	IEnumerator Reapper()
+     {
+         yield return new WaitForSeconds(4f);
+         myR.enabled = true;
+         myC.enabled = true;
+         //gameObject.SetActive(true);
+     }
+}
